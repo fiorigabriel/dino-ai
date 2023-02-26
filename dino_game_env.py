b20eb6c3124e3dcf5c8de4ecf2b193085cc69ac5
Gabriel Fiori <ggabrielfiori@gmail.com>
2023-02-26 11:29:35 -0300

    feat: implement the environment's step and reset methods

--- dino_game_env.py
+++ dino_game_env.py
@@ -9,18 +9,32 @@ from selenium.webdriver.common.keys import Keys
 
 
 class DinoGameEnv(gym.Env):
-  def __init__(self):
-    # 1 = jump, 2 = duck, 3 = do nothing
+  def __init__(self, headless=True):
+    # 0 = do nothing, 1 = jump, 2 = duck
     self.action_space = spaces.Discrete(3)
     self.observation_space = spaces.Box(low=-50, high=np.inf, shape=(9,), dtype=np.float32)
+    self.headless = headless
     self._setupWebdriver()
 
   def step(self, action):
-    pass
+    if action == 1:
+      self._sendKey(Keys.SPACE)
+    elif action == 2:
+      self._sendKey(Keys.DOWN)
+    else:
+      pass
+
+    observation = self.get_observation()
+    done = self._isGameOver()
+    reward = 1 if not done else -10
+
+    return observation, reward, done, {}
+
 
   def reset(self):
-    pass
-    
+    self.driver.execute_script("Runner.instance_.stop()")
+    self.driver.execute_script("Runner.instance_.restart()")
+    self.get_observation()
 
   def get_observation(self):
     return self._getGameObservation()
@@ -29,12 +43,16 @@ class DinoGameEnv(gym.Env):
     pass
 
   def close(self):
-    pass
+    self.driver.close()
 
   def _setupWebdriver(self):
     options = Options()
     options.add_argument("--mute-audio")
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    if self.headless:
+      options.add_argument("--headless")
+      options.add_argument("--disable-gpu")
+
     self.driver = webdriver.Chrome(options=options)
     self.driver.set_window_size(1600, 900)
 
@@ -44,7 +62,10 @@ class DinoGameEnv(gym.Env):
     except WebDriverException:
       pass
 
-    self.driver.find_element(By.TAG_NAME, "body").send_keys(Keys.SPACE)
+    self._sendKey(Keys.SPACE)
+
+  def _isGameOver(self):
+    return self.driver.execute_script("return Runner.instance_.crashed")
 
   def _getTrexInfo(self):
     return self.driver.execute_script("""const getTrexInfo = () => {
@@ -64,7 +85,6 @@ class DinoGameEnv(gym.Env):
   def _getNextObstacle(self):
     return self.driver.execute_script("""const getNextObstacle = () => {
       const { obstacles } = Runner.instance_.horizon;
-
       const obstacle = obstacles[0];
 
       return {
@@ -100,3 +120,6 @@ class DinoGameEnv(gym.Env):
       next_obstacle["height"],
       next_obstacle["width"],
     ]
+
+  def _sendKey(self, key):
+    self.driver.find_element(By.TAG_NAME, "body").send_keys(key)
